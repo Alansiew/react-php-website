6edfd80e0e45a0610e3afd7a56c343b910c156a6
Alan Siewiera <71738106+Alansiew@users.noreply.github.com>
2022-08-29 19:13:00 +0200

    Add files via upload

---
+++ php-backend/add_product.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require 'db_connection.php';
+include_once 'Product.php';
+include_once 'DVD.php';
+include_once 'Book.php';
+include_once 'Furniture.php';
+include_once 'manageData.php';
+$db_conn= new Database();
+
+
+$product = new manageData;
+
+
+$new=$product->insertProduct();
+
+if ($new) {
+    $dvd = $product->insertDVD();
+        }
+if($new){
+    $book=$product->insertBook();
+}
+if($new){
+    $furniture=$product->insertFurniture();
+}
+
+
+
+
+
+
+
+
+
+
