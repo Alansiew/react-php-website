402e86b7b0601b5314612b65952a6c5bbf93dda4
Alan Siewiera <71738106+Alansiew@users.noreply.github.com>
2022-09-12 12:13:50 +0200

    Update DVD.php

--- php-backend/DVD.php
+++ php-backend/DVD.php
@@ -2,6 +2,7 @@
 include_once 'db_connection.php';
 include_once 'Product.php';
 
+
 class DVD extends Product
 {
     private $size;
@@ -22,6 +23,55 @@ class DVD extends Product
     {
         $this->size = $size;
     }
+    public function insertProduct()
+    {
+        $db_conn = new Database();
+        $data = json_decode(file_get_contents("php://input"));
+        $sku = mysqli_real_escape_string($db_conn->con, trim($data->sku));
+        $name = mysqli_real_escape_string($db_conn->con, trim($data->name));
+        $price = mysqli_real_escape_string($db_conn->con, trim($data->price));
+        $type = mysqli_real_escape_string($db_conn->con, trim($data->type));
 
+        $product = new $type(null,null,null,null,null,null,null);
+        $product->setSku($sku);
+        $product->setName($name);
+        $product->setPrice($price);
+        $product->setType($type);
+        $sku=$product->getSku();
+        $name = $product->getName();
+        $price = $product->getPrice();
+        $type = $product->getType();
+        $add_product = mysqli_query($db_conn->con,
+            "INSERT INTO `products` ( `sku`, `name`,`price`,`type`) 
+            VALUES ('$sku','$name','$price','$type')");
+
+        $size = mysqli_real_escape_string($db_conn->con, trim($data->size));
+
+
+        $dvd = new DVD(null,null,null,null,null);
+        $dvd->setType($type);
+        $dvd->setSize($size);
+
+        $type = $dvd->getType();
+        $size = $dvd->getSize();
+        
+        $query = mysqli_query($db_conn->con,"SELECT ID FROM products WHERE ID = ( SELECT MAX(ID) FROM products)");
+        $result = mysqli_fetch_assoc($query);
+        $last_id = $result['ID'];
+
+
+       
+    if($add_product){
+        $insertProduct2 = mysqli_query($db_conn->con,
+            "INSERT INTO   dvd  ( `id`,`type`,`Size`) 
+            VALUES ('$last_id','$type','$size')  ");
+        if ($insertProduct2) {
+
+            echo json_encode(["success" => 1, "msg" => "Product Added.", "id" => $last_id]);
+        } else {
+            echo json_encode(["success" => 0, "msg" => "Product Not Added!"]);
+        }
+        }
+    }
 
 }
